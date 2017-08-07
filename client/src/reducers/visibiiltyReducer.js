72663a91f38b80e2c907b84f46cff671037466f2
davideliason <davethemaker@gmail.com>
2017-08-07 15:23:59 -0700

    create visibility filter reducer

---
+++ client/src/reducers/visibiiltyReducer.js
@@ -0,0 +1,13 @@
+
+const visibilityReducer = (state = 'SHOW_ALL', action) => {
+	switch(action.type){
+		case 'SET_VISIBILITY_FILTER':
+		   return {
+		   	action.filter
+		   }
+		default:
+			return state
+	}
+}
+
+export default visibilityReducer
