9446c817e25feeb5efad2850a1201a1c9ce5a180
davideliason <davethemaker@gmail.com>
2017-08-07 06:07:26 -0700

    successfully pass prop to Number presentational component, next step is to have that access reducer store

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Button from './Number.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h3>Read Books</h3>
+       <Button number="2"></Button>
       </div>
     );
   }
---
+++ client/src/components/Number.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+const Button = ({onClick, number }) => (
+		<button>{number}</button>
+	)
+
+export default Button
